14e899cf5b4ec475c8286161952dee45676ec2a2
ZeroMortalZen <79844633+ZeroMortalZen@users.noreply.github.com>
2023-02-17 16:43:37 +0000

    v0.5
    Mood/Stress levels System v0.1
    -----------------------------------
    able to store pain in var
    ---------------------------------
    
    bug fixes

--- app/src/main/java/com/linx/stress_free_app/PainFragment.java
+++ app/src/main/java/com/linx/stress_free_app/PainFragment.java
@@ -8,16 +8,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.slider.Slider;
+import com.linx.stress_free_app.StressSystem.PersonStressHelperClass;
 
 public class PainFragment extends Fragment {
 
    View view;
    TextView PainNumberView;
    Slider PainSlider;
-
+   Button PainSubmit;
+   PersonStressHelperClass stressHelperClass = new PersonStressHelperClass();
 
 
     @Override
@@ -28,15 +32,26 @@ public class PainFragment extends Fragment {
 
         PainSlider =(Slider) view.findViewById(R.id.PainSlider);
         PainNumberView =(TextView) view.findViewById(R.id.PainNumberView);
+        PainSubmit =(Button)view.findViewById(R.id.PainSubmit);
 
         PainSlider.addOnChangeListener(new Slider.OnChangeListener() {
             @Override
             public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
                 PainNumberView.setText(Float.toString(value));
+                stressHelperClass.setPain(Float.parseFloat(Float.toString(value)));
             }
         });
 
+        PainSubmit.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+                String getPain= String.valueOf(stressHelperClass.getPain());
+                Toast.makeText(getActivity(),getPain,Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/linx/stress_free_app/StressSystem/PersonStressHelperClass.java
@@ -0,0 +1,18 @@
+package com.linx.stress_free_app.StressSystem;
+
+public class PersonStressHelperClass {
+
+    float pain;
+
+    public float getPain(){return pain;}
+    public void setPain(float pain){this.pain= pain ;}
+
+    public PersonStressHelperClass(float pain){
+        this.pain= pain;
+    }
+
+    public PersonStressHelperClass(){
+
+    }
+
+}
