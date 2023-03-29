04bdb70a37f163942d9b095c12e5f0415972875d
ZeroMortalZen <79844633+ZeroMortalZen@users.noreply.github.com>
2023-03-30 00:07:22 +0100

    v0.7.8
    -Added MainMenu Fragment
    -switchable  fragment nav bar
    [Code Not Tested Yet]
    
    -Bug Fixes

--- app/src/main/java/com/linx/stress_free_app/MainMenu/MainMenuActivity.java
+++ app/src/main/java/com/linx/stress_free_app/MainMenu/MainMenuActivity.java
@@ -1,16 +1,82 @@
 package com.linx.stress_free_app.MainMenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
+import com.linx.stress_free_app.MainMenu.MainMenuFragments.ExerciseFragment;
+import com.linx.stress_free_app.MainMenu.MainMenuFragments.HomeFragment;
+import com.linx.stress_free_app.MainMenu.MainMenuFragments.MeditationFragment;
+import com.linx.stress_free_app.MainMenu.MainMenuFragments.MusicFragment;
+import com.linx.stress_free_app.ProfileActivity;
 import com.linx.stress_free_app.R;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+
+    private Fragment currentFragment;
+    FloatingActionButton fab ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) this);
+
+        // Load the first fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        currentFragment = new HomeFragment();
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, currentFragment).commit();
+        Intent intent = new Intent(this, ProfileActivity.class);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Starts Profile Activity
+                startActivity(intent);
+            }
+        });
     }
+
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        switch (item.getItemId()) {
+            case R.id.homeMenu:
+                currentFragment = new HomeFragment();
+                break;
+            case R.id.meditationMenu:
+                currentFragment = new MeditationFragment();
+                break;
+            case R.id.excerciseMenu:
+                currentFragment = new ExerciseFragment();
+                break;
+
+            case R.id.listenMenu:
+                currentFragment = new MusicFragment();
+                break;
+        }
+
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, currentFragment).commit();
+        return true;
+    }
+
+
 }
